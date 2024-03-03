a63128eaffcbb789a018a79c439c47182da6c34f
OlenaVovchenko <olenavovchenko@gmail.com>
2024-03-03 12:05:59 +0100

    Zadachi poslednii

--- untitled/src/main/java/ZadachiJava/Zadacha18.java
+++ untitled/src/main/java/ZadachiJava/Zadacha18.java
@@ -1,2 +1,13 @@
-package ZadachiJava;public class Zadacha18 {
+package ZadachiJava;
+
+public class Zadacha18 {
+    public static void main(String[] args) {
+        String str = "first meaning in the row";
+        String end = "!!!";
+        if (str.endsWith(end)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
 }
--- untitled/src/main/java/ZadachiJava/Zadacha19.java
+++ untitled/src/main/java/ZadachiJava/Zadacha19.java
@@ -1,2 +1,9 @@
-package ZadachiJava;public class Zadacha19 {
+package ZadachiJava;
+
+public class Zadacha19 {
+    public static void main(String[] args) {
+        String str = "After my birthday";
+        String strApper = str.toUpperCase();
+        System.out.println(strApper);
+    }
 }
